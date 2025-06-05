a1bb3a47b4e62527ec015e9b65e7fec8fa7e8055
SehrishHussain <sehrishaslam1@hotmail.com>
2025-06-05 09:39:09 +0500

    zod validation

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,10 @@
+import {z} from "zod"
+
+
+export const messageSchema = z.object({
+    content: z
+    .string()
+    .min(10, {message: 'Content must be at least of 10 chars'})
+    .max(300, {message: 'Content must be no longer than of 300 chars'})
+   
+})
---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,14 @@
+import {z} from 'zod'
+
+export const usernameValidation = z
+    .string()
+    .min(2, "Username must be atleast 2 characters")
+    .max(20, "Username must be not more than 20 chars")
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,"Username must not contain special chars")
+
+
+    export const signUpSchema = z.object({
+        username: usernameValidation,
+        email: z.string().email({message: 'Invalid email address'}),
+        password: z.string().min(6, {message: "password must be at least 6 characters"})
+    })
