12a67dc2c448d837e0f4e39dd21963d8877f49b8
SehrishHussain <sehrishaslam1@hotmail.com>
2025-06-24 07:24:43 +0500

    user model modified

--- src/model/CheckListItem.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema, model, models } from 'mongoose';
-
-const ChecklistItemSchema = new Schema({
-  valleyId: { type: Schema.Types.ObjectId, ref: 'Valley' },
-  title: String,
-  category: { type: String, enum: ['clothing', 'health', 'documents'] }
-});
-
-export default models.ChecklistItem || model('ChecklistItem', ChecklistItemSchema);
